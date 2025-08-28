3b9634eb6df3f1cc5056b9e8ce2598b6459d14c4
Linconwzx <linconkkjj@gmail.com>
2025-08-27 22:15:05 -0300

    Começo de uma api

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>OS cara do teto tão na minha maldade</Text>
       <StatusBar style="auto" />
     </View>
   );
